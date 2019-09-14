290562c692a40e00422c3496a5b3759cd48f9b2e
Charles Bentley <cbentley@mail.ru>
2019-09-14 18:06:06 +0200

    init commit

---
+++ src/pasa/cbentley/jpasc/swing/utils/tests/TestPascalSwingUtils.java
@@ -0,0 +1,11 @@
+package pasa.cbentley.jpasc.swing.utils.tests;
+
+import pasa.cbentley.testing.BentleyTestCase;
+
+public class TestPascalSwingUtils extends BentleyTestCase {
+
+   public void setupAbstract() {
+      
+   }
+
+}
